e91fc5275581aa561597c1726895448c71ba9783
alchemistlowkey <alchemistlowkey@gmail.com>
2023-08-14 14:54:22 -0700

    cPython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,18 +8,4 @@
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t length = 0, allocated, i = 0;
-	const char *type;
-
-	length = PyList_Size(p);
-	allocated = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %ld\n", length);
-	printf("[*] Allocated = %ld\n", allocated);
-
-	for (; i < length; i++)
-	{
-		PyObject *item = PyList_GetItem(p, i);
-		type = Py_TYPE(item)->tp_name;
-		printf("Element %ld: %s\n", i, type);
-	}	
 }
