aceaf98783b845fd821ed2305e2f861be817a517
alchemistlowkey <alchemistlowkey@gmail.com>
2023-08-28 14:35:48 -0700

    handled

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
             value += 1
-    except:
+    except IndentationError:
         pass
     finally:
         print()
