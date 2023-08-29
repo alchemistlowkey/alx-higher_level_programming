c7ec83d9a08343aa72bf623ab69de1d0c3324ff4
alchemistlowkey <alchemistlowkey@gmail.com>
2023-08-29 14:19:58 -0700

    empty square created

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Created an empty class square
+"""
+
+
 class Square:
-    """ A square is a shape with equal dimensions
+    """
+    A square is a shape with equal dimensions
+
+    Attributes:
+        empty
     """
     pass
