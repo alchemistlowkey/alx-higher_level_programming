9ed24bf8c7328321134c5261299d6b0167854bb1
alchemistlowkey <alchemistlowkey@gmail.com>
2023-08-29 14:32:40 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Created a class square
+"""
+
+
+class Square:
+    """
+    A square is a shape with equal dimensions
+
+    Attributes:
+        size (int): The size of the square
+    """
+
+    def __init__(self, size):
+        """
+        The __init__ method for initializing the square class
+
+        Args:
+            size: (:obj: 'init'): A private instance square size
+        """
+
+        self.__size = size
