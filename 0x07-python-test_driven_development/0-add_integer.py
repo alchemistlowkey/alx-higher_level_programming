33eb646549295f9ca12c5146f215708e9ff775f2
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-03 12:09:12 -0700

    addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -26,6 +26,7 @@ def add_integer(a, b=98):
             a = int(a)
         else:
             raise TypeError("a must be an integer")
+
     if type(b) is not int:
         if type(b) is float and b == b and abs(b) <= 1.797693e+308:
             b = int(b)
