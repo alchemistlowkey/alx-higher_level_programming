7d4e6b115f788923895d2e505b586465ddeab06d
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-04 14:29:38 -0700

    public instance of area and perimeter added

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -82,3 +82,30 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    def area(self):
+        """
+        Public Instance Method
+        Calculates the area of the rectangle
+
+        Returns:
+            The area of the rectangle
+        """
+
+        areaa = self.__width * self.__height
+        return areaa
+
+    def perimeter(self):
+        """
+        Public instance method
+        Calculates the perimeter of the rectangle
+
+        Returns:
+            The perimeter of the rectangle
+        """
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        peri = 2 * (self.__width + self.__height)
+        return peri
