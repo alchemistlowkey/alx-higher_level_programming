dfbe3b58990b695fa0b3d86ee5522a3bfce91483
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-11 17:21:30 -0700

    Instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Inheritance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    A function to check if an object is an instance of a class
+
+    Args:
+        obj: object
+        a_class: class
+
+    Returns:
+        True or False
+    """
+
+    return isinstance(obj, a_class)
