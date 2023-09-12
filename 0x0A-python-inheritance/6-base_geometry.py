c174c6aee210bf9a01f5d9af802d44084d474393
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-11 17:41:33 -0700

    Exception handled

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Class Base Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    A base geometry class
+    """
+
+    def area(self):
+        """
+        Public Instance method
+
+        Raises:
+            Exception: "area() is not implemented"
+        """
+
+        raise Exception("area() is not implemented")
