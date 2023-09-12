7880f297087fb027caefd3093eed2a456516a861
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-12 15:07:25 -0700

    JSON and Class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Student class
+"""
+
+
+class Student:
+    """
+    A class that defines a Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization method for Student class
+
+        Attributes:
+            first_name: The first name of the student
+            last_name: The last name of the student
+            age: The age of the student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Represents of Student into json format
+
+        Return:
+            Student class as a json format
+        """
+
+        if attrs is None:
+            return self.__dict__
+        return {key: value for (key, value) in self.__dict__.items()
+                if key in list(self.__dict__.keys())}
