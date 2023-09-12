64a3b328384f1028cf5f55808ba8d828f305b99d
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-12 15:44:47 -0700

    JSON

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Student class
+"""
+
+
+class Student:
+    """
+    A class that defines a Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization method for Student class
+
+        Args:
+            first_name: The first name of the student
+            last_name: The last name of the student
+            age: The age of the student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Represents of Student into json format
+
+        Args:
+            attrs: List of attributes names
+
+        Return:
+            Student class as a json format
+        """
+
+        if attrs is None:
+            return self.__dict__
+        js = {}
+        for i in attrs:
+            if i in self.__dict__:
+                js[i] = self.__dict__[i]
+        return js
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance based
+
+        Args:
+            json: A dictionary containing attribute-value pairs
+        """
+
+        for i, j in json.items():
+            setattr(self, i, j)
