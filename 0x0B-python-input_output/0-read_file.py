9773c721e7949e275af7c9e9ada436033cdccde4
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-12 11:44:32 -0700

    documentation added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,16 @@ Read file
 
 
 def read_file(filename=""):
+    """
+    A function that reads a text file and prints to stdout
+
+    Args:
+        filename: the file name that accepts the file read
+
+    Returns:
+        Nothing
+    """
+
     with open("my_file_0.txt", encoding="utf-8") as f:
         filename = f.read()
         print(filename, end="")
