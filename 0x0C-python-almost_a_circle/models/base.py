362da026775fec85a3833d0f3eab3abfc4ba6080
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-18 11:03:19 -0700

    empty list modified

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,7 +40,7 @@ class Base:
         """
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
