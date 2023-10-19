9ff7289f2facfe0730b3316d57c781cd0ba60896
alchemistlowkey <alchemistlowkey@gmail.com>
2023-10-18 17:17:40 -0700

    created user and database with select grant

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- A script that creates a user and a database
+CREATE USER
+IF NOT EXISTS user_0d_2@localhost
+	IDENTIFIED BY "user_0d_2_pwd";
+
+CREATE DATABASE
+IF NOT EXISTS hbtn_0d_2;
+
+GRANT SELECT ON hbtn_0d_2.* 
+TO user_0d_2@localhost;
