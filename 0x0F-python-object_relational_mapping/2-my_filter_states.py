94d2c0363ae9fc086fa53305c28cb32dc1cf3081
alchemistlowkey <alchemistlowkey@gmail.com>
2023-11-16 12:38:53 -0800

    mysql database

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    mydbcursor = mydb.cursor()
+    mydbcursor.execute("SELECT * FROM states "
+                       "WHERE name LIKE BINARY '{}' "
+                       "ORDER BY states.id ASC;".format(sys.argv[4]))
+    states = mydbcursor.fetchall()
+    for s in states:
+        print(s)
+    mydbcursor.close()
+    mydb.close()
