7096c5a3b1638d3689446caf4216458074b712ce
alchemistlowkey <alchemistlowkey@gmail.com>
2023-11-30 17:46:34 -0800

    modified file length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL
-# And displays the size of the body of the response
-
 curl -Is "$1" | grep Content-Length | cut -f2 -d' '
