0e998ddd8d4c40b68f0f40425f3ba2e78898d0b5
alchemistlowkey <alchemistlowkey@gmail.com>
2024-01-04 06:41:18 -0800

    completed task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const completedTasks = {};
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+
+    tasks.forEach(task => {
+      if (task.completed) {
+        if (completedTasks[task.userId]) {
+          completedTasks[task.userId]++;
+        } else {
+          completedTasks[task.userId] = 1;
+        }
+      }
+    });
+    console.log(completedTasks);
+  }
+});
