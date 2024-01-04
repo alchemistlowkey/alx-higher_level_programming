8ab8f6803f90ed5c5ee096e67be78a35cf429623
alchemistlowkey <alchemistlowkey@gmail.com>
2024-01-04 06:24:44 -0800

    webpage file storage

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
+});
